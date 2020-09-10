49cae4a30a19f87a63dd1eb61d0253979c3d165b
Joshua Harms <joshua@nozzlegear.com>
2020-09-10 15:40:20 -0500

    Fix tarball download/unzipping; move rtlamr to /usr/bin/rtlamr

--- setup.fish
+++ setup.fish
@@ -14,15 +14,19 @@ echo "Installing rtl-sdr tools..."
 apt install rtl-sdr -y
 
 set rtlamrUrl "https://github.com/bemasher/rtlamr/releases/download/v0.9.1/rtlamr_linux_amd64.tar.gz"
-set rtlamrZip "rtlamr.zip"
+set rtlamrZip "rtlamr.tar.gz"
 
 echo "Installing rtlamr executable from $rtlamrUrl"
 
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O $rtlamrZip
+wget $rtlamrUrl -O $rtlamrZip
 mkdir -p "bin"
-unzip $rtlamrZip -d "bin"
+tar xvzf $rtlamrZip -C "bin"
+chmod +x bin/rtlamr
+mv bin/rtlamr /usr/bin/rtlamr
 rm $rtlamrZip
+rm -r bin 
 
+echo "Installed rtlamr at /usr/bin/rtlamr"
 echo "Blacklisting drivers for Linux"
 
 bash -c ./blacklist-linux-driver.sh
